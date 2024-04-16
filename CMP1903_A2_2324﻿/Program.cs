b13e836cf3cecc16ef635e88cf898d7416dfd1df
Morgan <99492170+morganblackwell@users.noreply.github.com>
2024-04-16 14:22:51 +0100

    Revert "Removed file"
    This reverts commit eefdf18e5a94d02efdc5bbfc4ecf1a2c8490c55f.

---
+++ CMP1903_A2_2324﻿/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A2_2324
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /*
+             * Create a Game object and call its methods.
+             * Create a Testing object to verify the output and operation of the other classes.
+             */
+
+            /// <summary>
+            /// Infinitely creates game objects through the Testing class
+            /// Allows the user to choose how many times the dice are rolled.
+            /// </summary>
+
+            while (true) // Runs continuously 
+            {
+                Game game = new Game();
+                game.Menu();
+
+                Console.WriteLine("How many times should three dice be rolled?");
+
+                try
+                {
+                    int rollAmount = Int32.Parse(Console.ReadLine());
+
+                    for (int rollCount = 1; rollCount <= rollAmount; rollCount++)
+                    {
+                        Testing testOutputs = new Testing();
+                        testOutputs.Main();
+                        Console.WriteLine(); // Seperate different sets of rolls
+                    }
+                }
+
+                catch (FormatException) // Stops non integer values being accepted
+                {
+                    Console.WriteLine("Input must be an integer");
+                    Main(); // Resets Program to allow the user to enter a valid input
+                }
+
+                catch (OverflowException) // Stops values too big or small being accepted
+                {
+                    Console.WriteLine("Input value too large/small");
+                    Main();
+                }
+
+                catch (Exception exception) // Stop any other input errors
+                {
+                    Console.WriteLine($"Input error: {exception}");
+                    Main();
+                }
+            }
+        }
+    }
+}
